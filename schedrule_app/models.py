17500853996bec85f2869935b4f6405b2d59f3a4
Isaac Laffely <laffely.isaac@gmail.com>
2024-04-07 17:25:01 -0600

    I dont know why it is doing this

--- schedrule_app/models.py
+++ schedrule_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
+
+class EventType (models.Model):
+    COLOR = (
+        ('Blue', 'Blue'),
+        ('Red', 'Red'),
+        ('Green', 'Green'),
+        ('Yellow', 'Yellow')
+    ) 
+    name = models.CharField(max_length = 50)
+    color = models.CharField(max_length=200, choices=COLOR, blank = True)
+    description = models.TextField(blank=True)
+    
+     #Default String
+    def __str__(self):
+        return self.name
+    
+    
+    def get_absolute_url(self):
+        return reverse('EventType-detail', args=[str(self.id)]) 
+    
